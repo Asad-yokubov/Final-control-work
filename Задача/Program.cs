c15c491dadf66e78d86e696973158680364ef8e1
Asad-yokubov <for.home.work.2024@gmail.com>
2024-11-01 23:25:58 +0500

    задаём массив строк

---
+++ Задача/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] inputArray = {"Hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan"}; 
+    }
+} 

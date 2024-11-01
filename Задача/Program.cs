c648bbf3b1eebb31fd78de3b5109308840f5cf0c
Asad-yokubov <for.home.work.2024@gmail.com>
2024-11-01 23:30:07 +0500

    Считаем количество строк, которые соответствуют условию

--- Задача/Program.cs
+++ Задача/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main()
     {
         string[] inputArray = {"Hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan"}; 
+
+        int maxLength = 3;
+        int count = 0;
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= maxLength)
+            {
+                count++;
+            }
+        }
     }
 } 
